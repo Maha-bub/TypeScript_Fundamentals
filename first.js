3da0c9f0873d1c66c0b339225023d57bb9dcad15
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-12 22:43:25 +0600

    first typescript file created

---
+++ first.js
@@ -0,0 +1,13 @@
+"use strict";
+let student = {
+    name: "John Doe",
+    age: 20,
+    grade: "A"
+};
+let obj = {
+    name: "Mahabub",
+    age: 22,
+    grade: "B"
+};
+console.log(student);
+// console.log(obj)
---
+++ first.ts
@@ -0,0 +1,14 @@
+ let student: { name: string; age: number; grade: string } = {
+    name: "John Doe",
+    age: 20,
+    grade: "A"
+};
+
+ let obj: { name: string, age: number, grade: string } = {
+    name: "Mahabub",
+    age: 22,
+    grade: "B"
+}
+
+console.log(student);
+// console.log(obj)
