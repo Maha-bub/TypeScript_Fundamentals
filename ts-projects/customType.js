d74dc13345ef8ebe80c1a6053cf0dd4df30e05c0
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-13 11:15:13 +0600

    custom types aliaes

---
+++ ts-projects/customType.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const robin = {
+    name: "Robin",
+    age: 20,
+    rollNo: 101,
+    id: 1
+};
+//# sourceMappingURL=customType.js.map
---
+++ ts-projects/customType.ts
@@ -0,0 +1,13 @@
+type Student = {
+    name: string,
+    age: number,
+    rollNo: number
+    id: number
+}
+
+const robin: Student = {
+    name: "Robin",
+    age: 20,
+    rollNo: 101,
+    id: 1
+}
