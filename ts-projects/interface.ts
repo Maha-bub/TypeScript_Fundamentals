47dc3c6cf9328feef208c61816619e2224b6bafc
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-13 11:20:50 +0600

    practice how to use custom types , it's interface

---
+++ ts-projects/interface.ts
@@ -0,0 +1,13 @@
+interface Student {
+    Id: number
+    name: string
+    age: number
+    group: string
+}
+
+const Mahabub: Student = {
+    Id: 1,
+    name: 'Mahabub',
+    age: 20,
+    group: "A"
+}
