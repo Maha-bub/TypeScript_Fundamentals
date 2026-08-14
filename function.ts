b8d95e1ba98fcc94b82f54fdb712e1901d73072d
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-15 02:33:42 +0600

    function return type decrlare

--- function.ts
+++ function.ts
@@ -2,11 +2,11 @@ const addTowNumbers = (num1: number, num2: number): number[] => {
     const totalSum: number = num1 + num2;
     return [totalSum];
 }
-const substructTowNumbers = (num1: number, num2: number): number[] => {
+const substructTowNumbers = (num1: number, num2: number): { number } => {
     const total: number = num1 - num2;
-    return [total];
+    return { number: total };
 }
-const multiplayTowNumbers = (num1: number, num2: number): number[] => {
+const multiplayTowNumbers = (num1: number, num2: number): [string, number] => {
     const total: number = num1 * num2;
-    return [total];
+    return ["Total", total];
 }   
