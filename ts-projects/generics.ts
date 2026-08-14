de6022315ccf8cbe86d75365b70ef76432806348
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-14 18:19:12 +0600

    generics interface

---
+++ ts-projects/generics.ts
@@ -0,0 +1,14 @@
+interface Response<T> {
+    data: T,
+    status: 401 //not found
+}
+
+const transactionAmount: Response<number> = {
+    data: 50000,
+    status: 401
+}
+
+const userIsactive: Response<boolean> = {
+    data: true,
+    status: 401
+}
