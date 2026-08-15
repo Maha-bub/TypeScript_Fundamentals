61c57e0dc0c08a96fc882e4c682282231acbe9e3
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-16 01:08:25 +0600

    revice typscript 5 basic pillers

---
+++ revice.ts
@@ -0,0 +1,19 @@
+/**
+ * 1. variable
+ * 2.array declare
+ * 3.function
+ * 4.objects
+ * 
+ * */
+let names: string = "mahabub";
+let age: number = 32
+
+let persons: number[] = [12, 12, 14, 5, 2, 1, 23,]
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+const person: { name: string, age: number } = {
+    name: "mahabub",
+    age: 32
+}   
