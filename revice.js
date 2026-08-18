251d23564a66654f5dcc8de427e89881fde21c23
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-19 01:13:52 +0600

    compailed

---
+++ revice.js
@@ -0,0 +1,18 @@
+"use strict";
+/**
+ * 1. variable
+ * 2.array declare
+ * 3.function
+ * 4.objects
+ *
+ * */
+let names = "mahabub";
+let age = 32;
+let persons = [12, 12, 14, 5, 2, 1, 23,];
+function add(a, b) {
+    return a + b;
+}
+const person = {
+    name: "mahabub",
+    age: 32
+};
