eda1d3434cef4785477cb5b82c1d3f9bae948ecd
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-21 16:35:25 +0600

    update some code of line

--- typescript-problem-solving/src/renderNotification.ts
+++ typescript-problem-solving/src/renderNotification.ts
@@ -14,16 +14,12 @@ const renderNotification = (notification: LikeNotify | SystemNotify): string => 
     if (notification.actionUrl) {
         return `System: ${notification.message} (Tap to view)`;
     }
-
     return `System: ${notification.message}`;
 }
 
 
-console.log(renderNotification({ type: "like", fromUser: "Aisha" }));
-// "Aisha liked your post."
+console.log(renderNotification({ type: "like", fromUser: "Aisha" }));// "Aisha liked your post."
 
-console.log(renderNotification({ type: "system", message: "Maintenance complete." }));
-// "System: Maintenance complete."
+console.log(renderNotification({ type: "system", message: "Maintenance complete." }));// "System: Maintenance complete."
 
-console.log(renderNotification({ type: "system", message: "Your subscription is expiring soon.", actionUrl: "/billing" }));
-// "System: Your subscription is expiring soon. (Tap to view)"
+console.log(renderNotification({ type: "system", message: "Your subscription is expiring soon.", actionUrl: "/billing" }));// "System: Your subscription is expiring soon. (Tap to view)"
